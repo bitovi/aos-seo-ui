1d3ec5926e727013d315206bdf21e6e9b1211375
Sarayu Kosanam <skosanam@apple.com>
2016-07-19 14:44:44 -0700

    (feature/ST-20-seo-see-an-icon-to-export-data): corrected the indentation

--- src/pages/url-list/url-list.viewmodel.js
+++ src/pages/url-list/url-list.viewmodel.js
@@ -4,114 +4,125 @@ var Model = require('models/url/url.js');
 var rowTemplate = require('./row.stache');
 
 module.exports = can.Map.extend({
-  define: {
-    /**
-     * @property {Array{Object}} columns
-     * @description The list of columns (key name, header label, column width) used by the Grid List.
-     */
-    columns: {
-      value: [{
-        cssClass: 'col-md-2',
-        key: 'partNumber',
-        label: 'Part Number'
-      }, {
-        cssClass: 'col-md-4',
-        key: 'url',
-        label: 'URL'
-      }, {
-        cssClass: 'col-md-3',
-        key: 'pageTitle',
-        label: 'Page Title'
-      }, {
-        cssClass: 'col-md-1',
-        key: 'segment',
-        label: 'Segment'
-      }, {
-        cssClass: 'col-md-1',
-        key: 'region',
-        label: 'Region'
-      }, {
-        cssClass: 'col-md-1',
-        key: 'country',
-        label: 'Country'
-      }]
-    },
+    define: {
+        /**
+         * @property {Array{Object}} columns
+         * @description The list of columns (key name, header label, column width) used by the Grid List.
+         */
+        columns: {
+            value: [
+                {
+                    cssClass: 'col-md-2',
+                    key: 'partNumber',
+                    label: 'Part Number'
+                },
+                {
+                    cssClass: 'col-md-4',
+                    key: 'url',
+                    label: 'URL'
+                },
+                {
+                    cssClass: 'col-md-3',
+                    key: 'pageTitle',
+                    label: 'Page Title'
+                },
+                {
+                    cssClass: 'col-md-1',
+                    key: 'segment',
+                    label: 'Segment'
+                },
+                {
+                    cssClass: 'col-md-1',
+                    key: 'region',
+                    label: 'Region'
+                },
+                {
+                    cssClass: 'col-md-1',
+                    key: 'country',
+                    label: 'Country'
+                }
+            ]
+        },
 
-    /**
-     * @property {Array{Object}} dataOptions
-     * @description A list of search-able keys/columns, used by the Grid Search component.
-     */
-    dataOptions: {
-      value: [{
-        key: 'url',
-        label: 'URL'
-      }, {
-        key: 'pageTitle',
-        label: 'Page Title'
-      }, {
-        key: 'partNumber',
-        label: 'Part Number'
-      }]
-    },
+        /**
+         * @property {Array{Object}} dataOptions
+         * @description A list of search-able keys/columns, used by the Grid Search component.
+         */
+        dataOptions: {
+            value: [
+                {
+                    key: 'url',
+                    label: 'URL'
+                },
+                {
+                    key: 'pageTitle',
+                    label: 'Page Title'
+                },
+                {
+                    key: 'partNumber',
+                    label: 'Part Number'
+                }
+            ]
+        },
 
-    /**
-     * @property {can.Model} model
-     * @description The model used by the view.
-     */
-    model: {
-      get: function () {
-        return Model;
-      }
-    },
-    /**
-     * @property {Array} url-list.viewModel.actionBar actionBar
-     * @description Function wrappers for the action bar component.
-     */
-    actionBar: {
-      get: function () {
-        var viewModel = this;
-        var actionBar = [];
-        actionBar.push({
-          type: 'download',
-          title: 'Export',
-          dropDowns: [{
-            label: 'Export Nemo Ready File'
-          }, {
-            label: 'Export current view(.CSV)'
-          }]
-        });
-        return actionBar;
-      }
-    },
+        /**
+         * @property {can.Model} model
+         * @description The model used by the view.
+         */
+        model: {
+            get: function () {
+                return Model;
+            }
+        },
+        /**
+         * @property {Array} url-list.viewModel.actionBar actionBar
+         * @description Function wrappers for the action bar component.
+         */
+        actionBar: {
+            get: function () {
+                var viewModel = this;
+                var actionBar = [];
+                actionBar.push({
+                    type: 'download',
+                    title: 'Export',
+                    dropDowns: [{
+                        label: 'Export Nemo Ready File'
+                    },{
+                      label: 'Export current view(.CSV)'
+                    }]
+                });
+                return actionBar;
+            }
+        },
 
-    /**
-     * @property {function} rowTemplate
-     * @description Stores the template renderer function reference.
-     */
-    rowTemplate: {
-      value: function () {
-        return function () {
-          return rowTemplate;
-        };
-      }
-    },
+        /**
+         * @property {function} rowTemplate
+         * @description Stores the template renderer function reference.
+         */
+        rowTemplate: {
+            value: function () {
+                return function () {
+                    return rowTemplate;
+                };
+            }
+        },
 
-    /**
-     * @property {String} searchField
-     * @description The initial search key.
-     */
-    searchField: {
-      type: 'string',
-      value: 'url'
-    },
+        /**
+         * @property {String} searchField
+         * @description The initial search key.
+         */
+        searchField: {
+            type: 'string',
+            value: 'url'
+        },
 
-    /**
-     * @property {String} title
-     * @description The page's main header/title.
-     */
-    title: {
-      type: 'string',
-      value: 'URLs'
+        /**
+         * @property {String} title
+         * @description The page's main header/title.
+         */
+        title: {
+            type: 'string',
+            value: 'URLs'
+        }
     }
-  }
 });
