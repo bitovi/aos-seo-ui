61a77361b5b59e488d98280730cef7467d29e80a
Sarayu Kosanam <skosanam@apple.com>
2016-08-01 14:00:32 -0700

    (feature/update-the-env-urls): updated the model to call correct URL

--- src/models/url/url.js
+++ src/models/url/url.js
@@ -3,8 +3,8 @@ var can = require('can');
 require('can/map/define/define');
 var envVars = require('seo-ui/utils/environmentVars');
 module.exports = can.Model.extend({
-    findAll: 'GET' + envVars.apiUrl() + '/urls.json',
-    findOne: 'GET' + envVars.apiUrl() + '/urls/{url}.json'
+    findAll: 'GET ' + envVars.apiUrl() + '/urls.json',
+    findOne: 'GET ' + envVars.apiUrl() + '/urls/{url}.json'
 }, {
     define: {
         country: {
