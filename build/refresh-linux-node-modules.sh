715d4c0ede4aa7637ec2914a8f82e3c3c9aeb51a
Anand Varma <anand_varma@apple.com>
2016-08-03 14:03:39 -0700

    updating the refesh node modules to decrease the build time. Node modules gets copied to the Bamboo's mount location

--- build/refresh-linux-node-modules.sh
+++ build/refresh-linux-node-modules.sh
@@ -6,11 +6,11 @@ if [ ! -d "$BUILD_DIR" ]; then
 	SRC_DIR=$(dirname "$BUILD_DIR")
 fi
 
-MVN_ARTIFACT_NAME=node-modules-shff-linux-x64
+MVN_ARTIFACT_NAME=node-modules-seo-linux-x64
 ZIP_FILE=$MVN_ARTIFACT_NAME.zip
 
 MVN_GROUP_ID=com.apple.store.content
-MVN_VERSION=2.3-SNAPSHOT
+MVN_VERSION=1.0-SNAPSHOT
 
 # This might be too aggressive, but is needed to make sure we have 100% correct modules
 
@@ -36,23 +36,4 @@ echo "npm install on the module"
 npm install --python=/usr/bin/python26
 npm ls
 
-#
-# Build the zip file, and fly that to nexus
-#
-
-zip -r $ZIP_FILE node_modules/
-
-#
-# Fly the artifact to Nexus
-#
-
-# Check for the task. Used deploy:deploy-file for real deployment (setup in bamboo), else it will just do install
-if [ -z $MVN_TASK ]; then
-	MVN_TASK=deploy:deploy-file
-fi
-
-mvn $MVN_TASK -Dfile=$ZIP_FILE \
-  -DgeneratePom=true \
-  -DgroupId=$MVN_GROUP_ID -DartifactId=$MVN_ARTIFACT_NAME -Dversion=$MVN_VERSION -Dpackaging=zip \
-  -Durl=http://store-nexusrepo.apple.com/nexus/content/repositories/snapshots \
-  -DrepositoryId=snapshots
+rsync -azv --delete node_modules/ worun@nc1q-cibuild-1010.corp.apple.com:/nc1_storeci2_workspace/Bamboo/Resources/seo-ui/node_modules
