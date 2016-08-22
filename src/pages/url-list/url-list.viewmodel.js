f37c761093a08189c7f66471fd3b03426ca5cabb
Sarayu Kosanam <skosanam@apple.com>
2016-08-22 10:11:30 -0700

    (feature/ST-74-export-options-for-nemo-ready-file-.csv): updated the indentation of columns and dataoptions

--- src/pages/url-list/url-list.viewmodel.js
+++ src/pages/url-list/url-list.viewmodel.js
@@ -9,37 +9,45 @@ module.exports = can.Map.extend({
          * @property {Array<can.Map>} url-list.viewModel.columns columns
          * @description The list of columns (key name, header label, column width) used by the Grid List.
          */
-        columns: {
-            value: [{
-                cssClass: 'col-md-2',
-                key: 'partNumber',
-                label: 'Part Number'
-            }, {
-                cssClass: 'col-md-2',
-                key: 'url',
-                label: 'URL'
-            }, {
-                cssClass: 'col-md-2',
-                key: 'pageTitle',
-                label: 'Page Title'
-            }, {
-                cssClass: 'col-md-3',
-                key: 'description',
-                label: 'Description',
-                isHidden: true
-            }, {
-                cssClass: 'col-md-1',
-                key: 'segment',
-                label: 'Segment'
-            }, {
-                cssClass: 'col-md-1',
-                key: 'region',
-                label: 'Region'
-            }, {
-                cssClass: 'col-md-1',
-                key: 'country',
-                label: 'Country'
-            }]
+         columns: {
+            value: [
+                {
+                    cssClass: 'col-md-2',
+                    key: 'partNumber',
+                    label: 'Part Number'
+                },
+                {
+                    cssClass: 'col-md-2',
+                    key: 'url',
+                    label: 'URL'
+                },
+                {
+                    cssClass: 'col-md-2',
+                    key: 'pageTitle',
+                    label: 'Page Title'
+                },
+                {
+                    cssClass: 'col-md-3',
+                    key: 'description',
+                    label: 'Description',
+                    isHidden: true
+                },
+                {
+                    cssClass: 'col-md-1',
+                    key: 'segment',
+                    label: 'Segment'
+                },
+                {
+                    cssClass: 'col-md-1',
+                    key: 'region',
+                    label: 'Region'
+                },
+                {
+                    cssClass: 'col-md-1',
+                    key: 'country',
+                    label: 'Country'
+                }
+            ]
         },
 
         /**
@@ -56,16 +64,20 @@ module.exports = can.Map.extend({
          * @description A list of search-able keys/columns, used by the Grid Search component.
          */
         dataOptions: {
-            value: [{
-                key: 'url',
-                label: 'URL'
-            }, {
-                key: 'pageTitle',
-                label: 'Page Title'
-            }, {
-                key: 'partNumber',
-                label: 'Part Number'
-            }]
+            value: [
+                {
+                    key: 'url',
+                    label: 'URL'
+                },
+                {
+                    key: 'pageTitle',
+                    label: 'Page Title'
+                },
+                {
+                    key: 'partNumber',
+                    label: 'Part Number'
+                }
+            ]
         },
 
         /**
