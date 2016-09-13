64cb8a577d27ed474a2ff126ccac30d5636ce01e
Sarayu Kosanam <skosanam@apple.com>
2016-09-13 15:16:56 -0700

    (feature/ST-203-seo-filter-page-add-status-column): updated the indentation

--- src/models/url/url.js
+++ src/models/url/url.js
@@ -3,61 +3,64 @@ require('can/map/define/define');
 var can = require('can');
 var envVars = require('seo-ui/utils/environmentVars');
 
-module.exports = can.Model.extend({
-    findAll: 'GET ' + envVars.apiUrl() + '/urls.json',
-    findOne: 'GET ' + envVars.apiUrl() + '/urls/{url}.json'
-}, {
-    define: {
-        country: {
-            type: 'string'
-        },
+module.exports = can.Model.extend(
+    {
+        findAll: 'GET ' + envVars.apiUrl() + '/urls.json',
+        findOne: 'GET ' + envVars.apiUrl() + '/urls/{url}.json'
+    },
+    {
+        define: {
+            country: {
+                type: 'string'
+            },
 
-        description: {
-            type: 'string'
-        },
+            description: {
+                type: 'string'
+            },
 
-        descriptionKeyPath: {
-            type: 'string'
-        },
+            descriptionKeyPath: {
+                type: 'string'
+            },
 
-        descriptionUrl: {
-            type: 'string'
-        },
+            descriptionUrl: {
+                type: 'string'
+            },
 
-        pageTitle: {
-            type: 'string'
-        },
+            pageTitle: {
+                type: 'string'
+            },
 
-        pageTitleKeyPath: {
-            type: 'string'
-        },
+            pageTitleKeyPath: {
+                type: 'string'
+            },
 
-        pageTitleUrl: {
-            type: 'string'
-        },
+            pageTitleUrl: {
+                type: 'string'
+            },
 
-        partNumber: {
-            type: 'string'
-        },
+            partNumber: {
+                type: 'string'
+            },
 
-        region: {
-            type: 'string'
-        },
+            region: {
+                type: 'string'
+            },
 
-        segment: {
-            type: 'string'
-        },
+            segment: {
+                type: 'string'
+            },
 
-        status: {
-            type: 'string'
-        },
+            status: {
+                type: 'string'
+            },
 
-        url: {
-            type: 'string'
-        },
+            url: {
+                type: 'string'
+            },
 
-        urlAspenLink: {
-            type: 'string'
+            urlAspenLink: {
+                type: 'string'
+            }
         }
     }
-});
+);
