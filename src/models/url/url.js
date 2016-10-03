ff63a77ed8ab8071fc67998314a50fd3c16d5b4b
Sarayu Kosanam <skosanam@apple.com>
2016-10-03 14:44:37 -0700

    (feature/ST-202-seo-filter-page-add-date-column-and): corrected the indentation

--- src/models/url/url.js
+++ src/models/url/url.js
@@ -4,84 +4,87 @@ var can = require('can');
 var envVars = require('seo-ui/utils/environmentVars');
 var formatDate = require('pui/utils/formatDate');
 
-module.exports = can.Model.extend({
-    findAll: 'GET ' + envVars.apiUrl() + '/urls.json',
-    findOne: 'GET ' + envVars.apiUrl() + '/urls/{url}.json',
+module.exports = can.Model.extend(
+    {
+        findAll: 'GET ' + envVars.apiUrl() + '/urls.json',
+        findOne: 'GET ' + envVars.apiUrl() + '/urls/{url}.json',
 
-    getFilters: function () {
-        var url = envVars.apiUrl() + '/url-filters.json';
+        getFilters: function () {
+            var url = envVars.apiUrl() + '/url-filters.json';
 
-        return can.ajax({
-            url: url,
-            method: 'get',
-            dataType: 'json',
-            contentType: 'application/json'
-        });
-    }
-}, {
-    define: {
-        country: {
-            type: 'string'
-        },
+            return can.ajax({
+                url: url,
+                method: 'get',
+                dataType: 'json',
+                contentType: 'application/json'
+            });
+        }
+    },
+    {
+        define: {
+            country: {
+                type: 'string'
+            },
 
-        createDate: {
-            type: function (value) {
-                return formatDate(value);
+            description: {
+                type: 'string'
+            },
+
+            createDate: {
+                type: function (value) {
+                    return formatDate(value);
+                }
+            },
+
+            descriptionKeyPath: {
+                type: 'string'
+            },
+
+            descriptionUrl: {
+                type: 'string'
+            },
+
+            modifyDate: {
+                type: function (value) {
+                    return formatDate(value);
+                }
+            },
+
+            pageTitle: {
+                type: 'string'
+            },
+
+            pageTitleKeyPath: {
+                type: 'string'
+            },
+
+            pageTitleUrl: {
+                type: 'string'
+            },
+
+            partNumber: {
+                type: 'string'
+            },
+
+            region: {
+                type: 'string'
+            },
+
+            segment: {
+                type: 'string'
+            },
+
+            status: {
+                type: 'string'
+            },
+
+            url: {
+                type: 'string'
+            },
+
+            urlAspenLink: {
+                type: 'string'
             }
-        },
-
-        description: {
-            type: 'string'
-        },
-
-        descriptionKeyPath: {
-            type: 'string'
-        },
-
-        descriptionUrl: {
-            type: 'string'
-        },
-
-        modifyDate: {
-            type: function (value) {
-                return formatDate(value);
-            }
-        },
-
-        pageTitle: {
-            type: 'string'
-        },
-
-        pageTitleKeyPath: {
-            type: 'string'
-        },
-
-        pageTitleUrl: {
-            type: 'string'
-        },
-
-        partNumber: {
-            type: 'string'
-        },
-
-        region: {
-            type: 'string'
-        },
-
-        segment: {
-            type: 'string'
-        },
-
-        status: {
-            type: 'string'
-        },
-
-        url: {
-            type: 'string'
-        },
-
-        urlAspenLink: {
-            type: 'string'
         }
     }
-});
+);
