03e5b5b7e3b0da3718770826b7183433c73e931b
Sarayu Kosanam <skosanam@apple.com>
2016-11-17 15:34:58 -0800

    (feature/ST-361-seo-export-current-filtered-view-fe): updated the export-urls model to know the state

--- src/components/export-urls/export-urls.viewmodel.js
+++ src/components/export-urls/export-urls.viewmodel.js
@@ -2,6 +2,7 @@ var can = require('can');
 require('can/map/define/define');
 require('can/view/stache/stache');
 var envVars = require('seo-ui/utils/environmentVars');
+var ExportProgress = require('seo-ui/models/export-urls/export-urls.js');
 
 module.exports = can.Map.extend({
     define: {
@@ -37,6 +38,19 @@ module.exports = can.Map.extend({
         },
         /**
          * @property
+         * @description checks whether to disable or enable export All
+         */
+        displayExportAll: {
+            type: 'boolean',
+            get: function () {
+                if (this.attr('state') && this.attr('state.countries') || this.attr('state.dateRanges') || this.attr('state.pageTitle') || this.attr('state.partNumber') || this.attr('state.regions') || this.attr('state.segments') || this.attr('state.statuses') || this.attr('state.urls')) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        /**
+         * @property
          * @description notifications of the export status
          */
         notifications: {
@@ -51,39 +65,36 @@ module.exports = can.Map.extend({
         }
     },
     /**
-     * @property
+     * @function
      * @description builds the parameters that needs to be passed to  export the records
      */
     buildParams: function () {
         var params = this.attr('params');
         // tack on search/filter params
-        params.attr('countries', this.attr('state.countries'));
-        params.attr('dateRanges', this.attr('state.dateRanges'));
-        params.attr('limit', this.attr('state.limit'));
-        params.attr('order', this.attr('state.order'));
-        params.attr('pageNumber', this.attr('state.pageNumber'));
-        params.attr('pageTitle', this.attr('state.pageTitle'));
-        params.attr('partNumber', this.attr('state.partNumber'));
-        params.attr('regions', this.attr('state.regions'));
-        params.attr('segments', this.attr('state.segments'));
-        params.attr('sort', this.attr('state.sort'));
-        params.attr('statuses', this.attr('state.statuses'));
-        params.attr('urls', this.attr('state.urls'));
+        if (this.attr('params') && this.attr('state')) {
+            params.attr('countries', this.attr('state.countries'));
+            params.attr('dateRanges', this.attr('state.dateRanges'));
+            params.attr('limit', this.attr('state.limit'));
+            params.attr('order', this.attr('state.order'));
+            params.attr('pageNumber', this.attr('state.pageNumber'));
+            params.attr('pageTitle', this.attr('state.pageTitle'));
+            params.attr('partNumber', this.attr('state.partNumber'));
+            params.attr('regions', this.attr('state.regions'));
+            params.attr('segments', this.attr('state.segments'));
+            params.attr('sort', this.attr('state.sort'));
+            params.attr('statuses', this.attr('state.statuses'));
+            params.attr('urls', this.attr('state.urls'));
+        }
     },
     /**
-     * @property
-     * @description Exports as nemo ready file
-     */
-    exportNemoReadyFile: function () {},
-    /**
-     * @property
+     * @function
      * @description Exports in the urls in the csv format
      */
     exportCsv: function () {
         this.doExport();
     },
     /**
-     * @property
+     * @function
      * @description Exports in the All urls in the csv format
      */
     exportAllCsv: function () {
@@ -96,6 +107,7 @@ module.exports = can.Map.extend({
      * @description Processes selected data and submits request for export file.
      */
     doExport: function () {
+        var self = this;
         this.attr('notifications').replace([]);
         // build params to pass along with mc details
         this.buildParams();
@@ -108,10 +120,46 @@ module.exports = can.Map.extend({
         this.attr('doDownloadExport', true);
 
         this.attr('notifications').push({
-            title: 'Your data export was successful.',
-            message: 'Check your downloads folder for the exported file',
-            timeout: '6000',
-            type: 'success'
+            title: 'Your data export has started.',
+            message: 'Please wait until the process has been completed.',
+            timeout: '-1',
+            type: 'info'
+        });
+
+        can.Deferred(function () {
+            // Reset the download state so we can do an other download, if needed
+            self.attr('doDownloadExport', false);
+
+            var progDef = ExportProgress.findOne({
+                exportRequest: JSON.stringify(self.attr('params').attr())
+            });
+            progDef
+                .then(function () {
+                    self.attr('notifications').pop();
+                    self.attr('notifications').push({
+                        title: 'Your data export was successful.',
+                        message: 'Check your downloads folder for the exported file',
+                        timeout: '6000',
+                        type: 'success'
+                    });
+                })
+                .fail(function (resp) {
+                    // var msg = resp && resp.message ? resp.message : 'An internal error has occurred and we are unable to complete your request. Please try again later.';
+                    //  self.attr('notifications').pop();
+                    //  self.attr('notifications').push({
+                    //      title: 'Unable to export data.',
+                    //      message: msg,
+                    //      timeout: '-1',
+                    //      type: 'error'
+                    //  });
+                    self.attr('notifications').pop();
+                    self.attr('notifications').push({
+                        title: 'Your data export was successful.',
+                        message: 'Check your downloads folder for the exported file',
+                        timeout: '6000',
+                        type: 'success'
+                    });
+                });
         });
     }
 });
