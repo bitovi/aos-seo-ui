82ec3c1659d11b89e9f69f1d9433e3c2618dbd35
Sarayu Kosanam <skosanam@apple.com>
2017-03-06 14:18:32 -0800

    (feature/ST-446-sherlock-ui-migration-generate-page): using bootstrap grid system and updated the styles accordingly

--- src/pages/request-page/request-page.viewmodel.js
+++ src/pages/request-page/request-page.viewmodel.js
@@ -1,10 +1,5 @@
 require('can/map/define/define');
 
 var can = require('can');
-var $ = require('jquery');
 
-module.exports = can.Map.extend({
-    showFormattingReqsModal: function () {
-        $('#formatting-requirements-modal').modal('show');
-    }
-});
+module.exports = can.Map.extend({});
