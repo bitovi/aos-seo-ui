f162a082047e3b1fe881e630836f8b3ee987d456
Mate Gyorffy <mategyorffy@Mates-MacBook-Pro.local>
2017-03-06 17:13:22 -0800

    add tabs; add layout ant content for Enter URLs and Upload File sections(tabs)

--- src/pages/review-page/review-page.js
+++ src/pages/review-page/review-page.js
@@ -1,10 +1,13 @@
 var can = require('can');
 
-var ViewModel = require('./review-page.viewmodel');
 var template = require('./review-page.stache!');
+var ViewModel = require('./review-page.viewmodel');
 
 require('can/map/define/define');
 require('can/view/stache/stache');
+require('pui/components/file-upload/file-upload');
+require('pui/components/tabs/tabs');
+require('bootstrap/js/modal');
 
 module.exports = can.Component.extend({
     tag: 'seo-review-page',
--- src/pages/review-page/review-page.viewmodel.js
+++ src/pages/review-page/review-page.viewmodel.js
@@ -4,5 +4,29 @@ var can = require('can');
 var $ = require('jquery');
 
 module.exports = can.Map.extend({
-    define: {}
+    define: {
+    	/**
+         * @property {String} review-page.viewModel.startTab startTab
+         * @description the tab show when the page loads with pui-tabs component
+         */
+    	startTab: {
+			type: 'string',
+    		value: 'Enter URLs'
+    	},
+
+    	/**
+         * @property {Array<Object>} review-page.viewModel.tabsList tabsList
+         * @description list of tabs to show by the pui-tabs componenet
+         */
+    	tabsList: {
+    		value: [
+	    		{
+	    			name: 'Enter URLs'
+	    		},
+				{
+	    			name: 'Upload File'
+	    		}
+	    	]
+	    }
+    }
 });
