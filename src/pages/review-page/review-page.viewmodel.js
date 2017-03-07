622c25638f00dc14f429c08ccce02193123b79d4
Mate Gyorffy <mategyorffy@Mates-MacBook-Pro.local>
2017-03-06 17:25:00 -0800

    fix indentation

--- src/pages/review-page/review-page.viewmodel.js
+++ src/pages/review-page/review-page.viewmodel.js
@@ -1,32 +1,31 @@
 require('can/map/define/define');
 
 var can = require('can');
-var $ = require('jquery');
 
 module.exports = can.Map.extend({
     define: {
-    	/**
-         * @property {String} review-page.viewModel.startTab startTab
-         * @description the tab show when the page loads with pui-tabs component
-         */
-    	startTab: {
-			type: 'string',
-    		value: 'Enter URLs'
-    	},
+		/**
+		 * @property {String} review-page.viewModel.startTab startTab
+		 * @description the tab show when the page loads with pui-tabs component
+		 */
+        startTab: {
+            type: 'string',
+            value: 'Enter URLs'
+        },
 
-    	/**
-         * @property {Array<Object>} review-page.viewModel.tabsList tabsList
-         * @description list of tabs to show by the pui-tabs componenet
-         */
-    	tabsList: {
-    		value: [
-	    		{
-	    			name: 'Enter URLs'
-	    		},
-				{
-	    			name: 'Upload File'
-	    		}
-	    	]
-	    }
+		/**
+		 * @property {Array<Object>} review-page.viewModel.tabsList tabsList
+		 * @description list of tabs to show by the pui-tabs componenet
+		 */
+        tabsList: {
+            value: [
+                {
+                    name: 'Enter URLs'
+                },
+                {
+                    name: 'Upload File'
+                }
+            ]
+        }
     }
 });
