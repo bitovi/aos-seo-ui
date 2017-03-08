6aabfeb214eac69745a44d1a7b8e7d6102c1b258
Sarayu Kosanam <skosanam@apple.com>
2017-03-07 17:09:26 -0800

    (feature/ST-446-sherlock-ui-migration-generate-page): updated the pui modal related code

--- src/pages/generate-page/generate-page.js
+++ src/pages/generate-page/generate-page.js
@@ -7,7 +7,6 @@ require('can/map/define/define');
 require('can/view/stache/stache');
 require('pui/components/file-upload/file-upload');
 require('pui/components/modal/modal');
-require('bootstrap/js/modal');
 
 module.exports = can.Component.extend({
     tag: 'seo-generate-page',
--- src/pages/generate-page/generate-page.viewmodel.js
+++ src/pages/generate-page/generate-page.viewmodel.js
@@ -2,4 +2,14 @@ require('can/map/define/define');
 
 var can = require('can');
 
-module.exports = can.Map.extend({});
+module.exports = can.Map.extend({
+    define: {
+        modalOpen: {
+            type: 'boolean',
+            value: false
+        }
+    },
+    toggleModal: function () {
+        this.attr('modalOpen', !this.attr('modalOpen'));
+    }
+});
