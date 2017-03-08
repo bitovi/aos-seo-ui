279e3d0e4624191419257aa098d72e417e471dd3
Sarayu Kosanam <skosanam@apple.com>
2017-03-07 17:34:50 -0800

    (feature/ST-446-sherlock-ui-migration-generate-page): updated the docs

--- src/pages/generate-page/generate-page.viewmodel.js
+++ src/pages/generate-page/generate-page.viewmodel.js
@@ -4,12 +4,20 @@ var can = require('can');
 
 module.exports = can.Map.extend({
     define: {
+        /**
+         * @property generate-page.viewmodel.modalOpen
+         * @description This is used to identify the state whether to show or hide the modal
+         */
         modalOpen: {
             type: 'boolean',
             value: false
         }
     },
 
+    /**
+     * @function generate-page.viewmodel.toggleModal
+     * @description The modal that toggles the display of an overlay of formatting requirements.
+     */
     toggleModal: function () {
         this.attr('modalOpen', !this.attr('modalOpen'));
     }
